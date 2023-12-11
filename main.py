75ea2156c1371e84a2ba81f4041458012b0ee779
JTuratkhan <108810678+JTuratkhan@users.noreply.github.com>
2023-12-11 00:42:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ from pyspark.sql.functions import count
 # Initialize Spark Session
 spark = SparkSession.builder.appName("BirdDataProcessing").getOrCreate()
 
-file_path = "dbfs:/FileStore/tables/your-output-directory/part-00000-tid-6687512951801856408-77285f10-1c38-4edc-92e1-f3f07f756634-9-1-c000.snappy.parquet"
+file_path = (
+    "dbfs:/FileStore/tables/your-output-directory/"
+    "part-00000-tid-6687512951801856408-77285f10-1c38-4edc-92e1-"
+    "f3f07f756634-9-1-c000.snappy.parquet"
+)
 
 # Read data from the Parquet file into a DataFrame
 birds_df = spark.read.parquet(file_path)
 
 # Group by 'scientific name' and count the occurrences
-transformed_df = birds_df.groupBy("scientific_name").agg(count("class_id").alias("image_count"))
+transformed_df = (
+    birds_df.groupBy("scientific_name")
+    .agg(count("class_id").alias("image_count"))
+)
 
 # Write the transformed data to the sink
 output_path = "dbfs:/FileStore/tables/your-output-directory/transformed/"
